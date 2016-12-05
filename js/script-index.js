eed41ac2e4a12782116e01f9ae26e62137f4dc29
MarianaPacheco <pachecoramirez.mariana@gmail.com>
2016-12-05 16:52:53 -0600

    Etapa 4

--- js/script-index.js
+++ js/script-index.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready( function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
@@ -33,16 +35,12 @@ function renderHighlightedRecipes(recipesArray) {
 	//FORMA 2 Mandar llamar el arreglo con each
 	/*_.each(recipesArray, renderRecipe);*/
 
+	//Búsqueda por propiedad highlighted
 	var objectRecipe = _.where(recipesArray, {highlighted:true});
 	console.log(objectRecipe);
-
-	renderRecipe(objectRecipe);
-
-	/*$.each(recipesArray, function (ind, objectRecipe) { 
-	  console.log('¡Hola :'+objectRecipe+'!'); 
-	}); */
-
-	
+	for (i=0; i < objectRecipe.length; i++){ 
+		renderRecipe(objectRecipe);
+	}
 };
 
 
@@ -53,10 +51,68 @@ function renderHighlightedRecipes(recipesArray) {
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
-	console.log('Voy a pintar la receta: ', recipe);
+	
+	
+		console.log('Voy a pintar la receta: ', recipe);
+		var vinculo = $('<a></a>');
+		vinculo.addClass('item-recipe');
+
+		var span1 = $('<span></span>');
+		span1.addClass('attribution');
+
+		var span2 = $('<span></span>');
+		span2.addClass('title-recipe');
+		span2.text(recipe[i].title);
+
+		var span3 = $('<span></span>');
+		span3.addClass('metadata-recipe');
+
+		var span4 = $('<span></span>');
+		span4.addClass('author-recipe');
+		span4.text(recipe[i].source.name);
+
+		var span5 = $('<span></span>');
+		span5.addClass('bookmarks-recipe');
+
+		var span6 = $('<span></span>');
+		span6.addClass('icon-bookmark'); 
+
+		var img = $('<img/>');
+		img.attr('src', recipe[i].source.url);
+
+		vinculo.append(span1);
+		span1.append(span2);
+		span1.append(span3);
+		span3.append(span4);
+		span3.append(span5);
+		span5.append(span6);
+		vinculo.append(img);
+
+	//console.log(vinculo);
+	//Pintarlos en la pantalla
+	$('.list-recipes').append(vinculo);
+
+	/*var template =
+	'<a class="item-recipe">' +
+		'<img src="<%= mainPicture %>">' +
+		'<p><%= name %></p>' +
+		'<p><%= age.number %>' +
+			'<span><%= age.type %></span>' +
+		'</p>' +
+	'</a>';
+
+	var compiled = _.template(template);
+	var li = compiled(pet);
+	console.log(li);
+
+	var elemento = $(li);
+	$('#petLi').append(elemento);*/
+
+
+
+
+
 }
-
-
 
 /*
 * Función que se encarga de pintar todas las actividades
